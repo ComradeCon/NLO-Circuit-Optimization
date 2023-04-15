e1568f8c3a044fae719ac172a4420098ba587064
ComradeCon <conrad.jensen@icloud.com>
2023-04-15 13:53:42 -0500

    s

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,10 +1,6 @@
 import atexit
 
 class WindowsInhibitor:
-    '''Prevent OS sleep/hibernate in windows; code from:
-    https://github.com/h3llrais3r/Deluge-PreventSuspendPlus/blob/master/preventsuspendplus/core.py
-    API documentation:
-    https://msdn.microsoft.com/en-us/library/windows/desktop/aa373208(v=vs.85).aspx'''
     ES_CONTINUOUS = 0x80000000
     ES_SYSTEM_REQUIRED = 0x00000001
 
